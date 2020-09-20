e00bce3de19d27ba4bb161cfe8b0b68c0768bf45
mizan <nickty.86@gmail.com>
2020-09-20 11:07:37 +0600

    plugin ready just before include folder

--- allyright-access.php
+++ allyright-access.php
@@ -22,10 +22,19 @@
  final class Allyright {
 
     /**
+     * Definding version of the plugin
+     */
+    const $version = '1.0'; 
+    
+    /**
      * Constuctor
      */
      private function __construct(){
+            $this->define_constants(); 
 
+            register_activation_hook( __FILE__, [$this, 'activate'] ); 
+
+            add_action( 'plugins_loaded', [$this, 'init_plugin'] ); 
      }
 
      /**
@@ -36,10 +45,41 @@
      public static function init(){
         static $instance = false; 
         if (! $instance) {
-            $instance = new selft(); 
+            $instance = new self(); 
         }
 
         return $instance; 
+     }
+
+     public function define_constants(){
+         define('ALLYRIGHT_VERSION', self::version); 
+         define('ALLYRIGHT_FILE', __FILE__); 
+         define('ALLYRIGHT_PATH', __DIR__); 
+         define('ALLYRIGHT_URL', plugins_url( '', ALLYRIGHT_FILE )); 
+         define('ALLYRIGHT__ASSETS', ALLYRIGHT_URL . '/assets' ); 
+     }
+
+     /**
+      * inintialize the plugin after loaded 
+      */
+
+     public function init_plugin(){
+         
+     }
+
+     /**
+      * Do somework when plugin is installing
+      * return void
+      */
+
+     public function activate(){
+
+        $installed = get_option('allyright_installed'); 
+
+        if (!installed) {
+            update_option( 'allyright_installed', time() );
+        }
+            update_option( 'allyright_version', ALLYRIGHT_VERSION); 
      }
  }
 
